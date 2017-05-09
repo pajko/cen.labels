cb4bf3e527dce4ab3807f7af1d8c8b9fe62b52ac
Patrik Lekaj <partik.lekaj@cenosco.com>
2017-05-09 11:21:45 +0200

    first commit for cen.labels

---
+++ cen.labels.component.js
@@ -0,0 +1,22 @@
+﻿ko.components.register('cen-labels', {
+    viewModel: {
+        createViewModel: function (params, componentInfo) {
+            var model = new CenLabelsModel(params.options);
+            if (params.options.hasOwnProperty('model')) {
+                params.options.model = model;
+            }
+            return model;
+        }
+    },
+    template: { element: 'LabelsTemplate' }
+});
+
+function CenLabelsModel(params) {
+    "use strict";
+    this.TestLabel1 = ko.observable(params.test1 || '');
+    this.TestLabel2 = params.test2 || '';
+
+    this.ChangeTest1 = function () {
+        this.TestLabel1('AAA');
+    }
+}
